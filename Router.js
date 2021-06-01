a71e56ea6d422ea91ba47060450f6556f36b7344
agen4ik <katya.risunova@gmail.com>
2021-06-01 08:42:00 +0300

    fix hrefs

--- Router.js
+++ Router.js
@@ -1,46 +1,3 @@
-// import Controller from './Controller.js';
-//
-//
-//
-// function getRouteInfo() {
-//     const hash = location.hash ? location.hash.slice(1) : '';
-//     const [name, id] = hash.split('/');
-//     return {name, params: {id}}
-// }
-//
-// function hadleHash(href){
-//     const {name, params} = getRouteInfo();
-//     if (name) {
-//         const routeName = name + 'Route';
-//         if (href.count){
-//             href.count -=1;
-//         }
-//         else{
-//             href = {route: name, params: params, count: 0};
-//             const temp = Controller[routeName](params);
-//             if (temp){
-//                 href = temp;
-//                 href.count = 1;
-//             }
-//
-//             window.location.href = window.location.href.split('#').slice(0, -1)
-//                 + '#' + href.route;
-//             if (href.params.id){
-//                 window.location.href += '/' + href.params.id;
-//             }
-//         }
-//     }
-//     return href;
-// }
-//
-// export default {
-//     init(){
-//         addEventListener('hashchange', function(e, href){
-//             href = hadleHash(href);
-//         });
-//         hadleHash(href);
-//     }
-// }
 import Controller from './Controller.js';
 
 function getRouteInfo() {
@@ -62,7 +19,7 @@ export default {
     init(){
         addEventListener('hashchange', hadleHash);
         if (!location.hash){
-            window.location.href += '/#games';
+            window.location.href += '#games';
         }
         hadleHash();
 
